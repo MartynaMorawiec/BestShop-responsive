6e5e8a85507ae3935bd3a34f1725208711bec0fe
Martyna <martyna.jasinska1@gmail.com>
2022-05-05 15:36:08 +0200

    final version

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 // quantity
-
 let price = {
   products: 0,
   orders: 0,
@@ -11,25 +10,18 @@ let price = {
   }
 };
 
-
-
-
 function calculateTotal() {
   const totalPrice = document.querySelector('#total-price');
 
   if (price.total()) {
     totalPrice.style.display = 'flex';
     totalPrice.querySelector('.total__price').innerText = `$${price.total()}`;
-
   } else {
     totalPrice.style.display = 'none';
-
   }
 }
 
-
 // products 
-
 document.querySelector('#products').addEventListener('input', (event) => {
   handleCalculations('products', event.target.value, 0.5);
 });
@@ -38,17 +30,13 @@ document.querySelector('#orders').addEventListener('input', (event) => {
   handleCalculations('orders', event.target.value, 0.25);
 });
 
-
 function handleCalculations(type, value, number) {
-
   const total = document.querySelector(`[data-id=${type}]`);
 
   if (value > 0) {
     total.style.display = 'flex';
-
     total.querySelector('.item__calc').innerText = `${value} * $${number}`;
     total.querySelector('.item__price').innerText = `$${value * number}`;
-
     price[type] = parseFloat(value) * number;
 
   } else if (value < 0) {
@@ -56,35 +44,26 @@ function handleCalculations(type, value, number) {
     total.querySelector('.item__calc').innerText = "";
     total.querySelector('.item__price').innerText = `Value should be greater than 0`;
 
-
   } else {
     total.style.display = 'none';
     price[type] = 0;
   }
-
   calculateTotal();
-
 }
-
 
 // package
 document.querySelector('#package').addEventListener('click', (event) => {
-
   const dropdown = document.querySelector('.select__dropdown');
 
   if (dropdown.style.display === 'none') {
     dropdown.style.display = 'block'; 
     document.querySelector('#package').classList.add("open");
 
-
   } else {
     dropdown.style.display = 'none';
     document.querySelector('#package').classList.remove("open");
   }
-
 });
-
-
 
 document.querySelectorAll('[data-value]').forEach(li => {
   li.addEventListener('click', (event) => {
@@ -92,48 +71,34 @@ document.querySelectorAll('[data-value]').forEach(li => {
     if (li.dataset.value === 'basic'){
       packagePrice(event.target.dataset.value, 0);
       
-
     } else if (li.dataset.value === 'professional') {
-      packagePrice(event.target.dataset.value, 25);
-     
+      packagePrice(event.target.dataset.value, 25); 
       
     } else if (li.dataset.value === 'premium') {
       packagePrice(event.target.dataset.value, 60);
-  
     } 
     calculateTotal();
-
   });
-
 });
 
-
 function packagePrice(value, number) {
-  const packageLi = document.querySelector("[data-id='package']");
-    
+  const packageLi = document.querySelector("[data-id='package']");  
   packageLi.style.display = 'flex';
   document.querySelector('#package').firstElementChild.innerText = `${value}`;
-
   packageLi.querySelector('.item__calc').innerText = 'Basic';
   price.package = number;
   packageLi.querySelector('.item__price').innerText = `$${price.package}`;
-
 }
 
-
-
 // accounting
-
 document.querySelector('#accounting').addEventListener('change', (event) => {
  checkboxPrice(event.target.checked, "accounting");
 });
-
 
 // rental terminal
 document.querySelector('#terminal').addEventListener('change', (event) => {
   checkboxPrice(event.target.checked, "terminal");
 });
-
 
 function checkboxPrice(checked, type) {
   const checkboxTotal = document.querySelector(`[data-id=${type}]`);
@@ -145,7 +110,6 @@ function checkboxPrice(checked, type) {
     price[type] = 0;
   }
   calculateTotal();
-
 }
 
 
